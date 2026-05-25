958ee564770fb6e94d10846d75dd972a10604f99
Satish Rathod <sr9022069@gmail.com>
2026-05-25 15:23:40 +0530

    upadte

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { LogOut, Upload, User as UserIcon, Heart, Shield } from 'lucide-react';
 
